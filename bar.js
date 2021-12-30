7615169b89475fd9c20cf631cec665a1b79427de
manish reddy <manishreddyalugonda@gmail.com>
2021-12-30 17:56:49 +0530

    commit

---
+++ bar.js
@@ -0,0 +1,61 @@
+
+var chart = new ej.charts.Chart({
+    //Initializing Primary X Axis
+    primaryXAxis: {
+        valueType: "Category",
+        title: "Months"
+    },
+    //Initializing Primary Y Axis
+    primaryYAxis: {
+        title: "Number of Visitors"
+    },
+    //Initializing Chart Series
+    series: [
+        {
+            type: "Bar",
+            dataSource: [
+                { name: "Edwards", revenue: 35000 },
+                { name: "Mulkiteo", revenue: 40000 },
+                { name: "bothel", revenue: 45000 },
+                { name: "Lynnwood", revenue: 50000 },
+                { name: "Seatle", revenue: 80000 },
+                { name: "Everett", revenue: 100000 },
+            ],
+            xName: "name",
+            yName: "revenue"
+        }
+    ]
+});
+chart.appendTo("#container1");
+
+
+var chart1 = new ej.charts.Chart({
+    //Initializing Primary X Axis
+    primaryXAxis: {
+        valueType: "Category",
+        title: "Months"
+    },
+    //Initializing Primary Y Axis
+    primaryYAxis: {
+        title: "Number of Visitors"
+    },
+    //Initializing Chart Series
+    series: [
+        {
+            type: "Bar",
+            dataSource: [
+                { name: "Material Sale", revenue: 10000 },
+                { name: "Maintainance", revenue: 45000 },
+                { name: "HWT Replacement", revenue: 55000 },
+                { name: "Bid Work Plumbing", revenue: 75000 },
+                { name: "Service HVAC", revenue: 80000 },
+                { name: "Bid Work HVAC", revenue: 120000 },
+                { name: "Service Plumbing", revenue: 200000 }
+            ],
+            xName: "name",
+            yName: "revenue"
+        }
+    ]
+});
+
+chart1.appendTo("#container2");
