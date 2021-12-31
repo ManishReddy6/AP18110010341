56c77572b816c7f81f2e08bb13d8d6b149c2d02b
manish reddy <manishreddyalugonda@gmail.com>
2021-12-31 07:59:30 +0530

    commit

--- app.js
+++ app.js
@@ -14,18 +14,45 @@ app.get('/homePage',function(req,res) {
   res.render('homePage');
 });
 
+function validate(pass)
+{
+  let c1=0,c2=0;
+  for(var i in pass)
+  {
+    if(pass[i]=='@' || (pass[i]>='A' && pass[i]<='Z') || (pass[i]>='a' && pass[i]<='z') || (pass[i]>='0' && pass[i]<='9'))
+    {
+      if(pass[i]>='A' && pass[i]<='Z')
+      {
+        c1=1;
+      }
+      if(pass[i]>='0' && pass[i]<='9')
+      {
+        c2=1;
+      }
+    }
+    else
+    {
+      return 0;
+    } 
+  }
+  if(c1==1 && c2==1)
+  {
+    return 1;
+  }
+  return 0;
+}
 
 app.post('/', (req,res)=>{
   const username=req.body.email;
   const pass=req.body.password;
   var format=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if(format.test(username) && pass==="SmartServTest@123" )
+  if(format.test(username) && validate(pass)==1 && pass==="SmartServTest@123" )
   {
-  	res.redirect('/homePage')
+  	res.redirect('/homePage');
   }
   else
   {
-  	res.redirect('/');	
+    res.redirect('/');  
   }
 });
 
